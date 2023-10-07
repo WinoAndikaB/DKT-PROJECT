adb6b89437099dae771eea72ac744bb9c5d1e57b
WinoAndikaB <107344071+WinoAndikaB@users.noreply.github.com>
2023-10-07 16:33:10 +0700

    Perbaikan Header

--- resources/views/home/katalogDonasi.blade.php
+++ resources/views/home/katalogDonasi.blade.php
@@ -73,21 +73,8 @@
       <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="../assets/img/blurred-image-1.jpg">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
-              <i class="fab fa-twitter"></i>
-              <p class="d-lg-none d-xl-none">Twitter</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
-              <i class="fab fa-facebook-square"></i>
-              <p class="d-lg-none d-xl-none">Facebook</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
-              <i class="fab fa-instagram"></i>
-              <p class="d-lg-none d-xl-none">Instagram</p>
+            <a class="nav-link" rel="tooltip" data-placement="bottom" href="/profileUser" target="_blank">
+              <i>Profil Anda</i>
             </a>
           </li>
           <li class="nav-item dropdown">
